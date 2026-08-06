a914c879f8c361851fd12fffe8488c9b89791c83
Krish Poddar <krish4031poddar@gmail.com>
2026-08-06 23:36:16 +0530

    Minor additions

--- lib/tfjs.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@tensorflow/tfjs" {
-  export function loadLayersModel(url: string): Promise<unknown>;
-  export function tidy<T>(fn: () => T): T;
-  export function zeros(shape: number[]): unknown;
-  export function tensor4d(values: unknown, shape: number[]): unknown;
-}
